ffe5c4d897426c2f6ff43cb5f7e7ca694a0c422b
Diba Feiznia <72469950+feiznia@users.noreply.github.com>
2024-09-08 14:31:57 +0330

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -3,7 +3,7 @@ import secrets
 import string
 
 
-def generate_password(length, nums, special_chars, uppercase, lowercase):
+def generate_password(length = 16, nums = 1, special_chars = 1, uppercase = 1, lowercase = 1):
    
     # Define the possible characters for the password
     letters = string.ascii_letters
@@ -34,3 +34,7 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
             break
     
     return password
+   
+if __name__ == '__main__':
+    new_password = generate_password()
+    print('Generated password:', new_password)
